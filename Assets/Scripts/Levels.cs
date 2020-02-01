9af8fcb8ddab5fe1e18d133a930cbe5cdd1d21d6
atil <hyperion91@gmail.com>
2020-02-01 20:25:17 +0100

    Procgen stuff

--- Assets/Scripts/Levels.cs
+++ Assets/Scripts/Levels.cs
@@ -4,21 +4,11 @@ public partial class GameStates
 {
     void InitLevels()
     {
-        var randomColors = new[]
-        {
-            RandomColor(),
-            RandomColor(),
-            RandomColor(),
-            RandomColor(),
-            RandomColor(),
-            RandomColor(),
-            RandomColor(),
-            RandomColor(),
-            RandomColor(),
-        };
-
         levels = new Level[]
         {
+            GenerateLevel(),
+            GenerateLevel(),
+            GenerateLevel(),
             new Level
             {
                 target = new MyTransform
@@ -45,13 +35,49 @@ public partial class GameStates
             },
         };
 
-        for (int i = 0; i < levels.Length; i++)
-        {
-            levels[i].bgColor = randomColors[i];
-            levels[i].spriteColor = Complement(randomColors[i]);
-        }
+        
     }
 
-    Color Complement(Color c) => new Color(1 - c.r, 1 - c.g, 1 - c.b, 1);
-    Color RandomColor() => new Color(Random.Range(0.3f, 0.9f), Random.Range(0.3f, 0.9f), Random.Range(0.3f, 0.9f), 1f);
+    Level GenerateLevel()
+    {
+        int targetX = Random.Range(1, Screen.width / 100 - 1);
+        int targetY = Random.Range(1, Screen.width / 100 - 1);
+        int targetRot = Random.Range(-3, 4);
+        int targetScale = Random.Range(1, 5);
+
+        int playerX = Random.Range(1, Screen.width / 100 - 1);
+        int playerY = 10 - playerX;
+        int playerRot = Random.Range(-3, 4);
+        int playerScale = Random.Range(1, 5);
+
+        const float valueMin = 0.3f;
+        const float valueMax = 0.9f;
+
+        Color bgColor = new Color(Random.Range(valueMin, valueMax), 
+            Random.Range(valueMin, valueMax), 
+            Random.Range(valueMin, valueMax), 
+            1f);
+        Color spriteColor = new Color(1 - bgColor.r, 1 - bgColor.g, 1 - bgColor.b, 1);
+
+        return new Level
+        {
+            target = new MyTransform
+            {
+                x = targetX,
+                y = targetY,
+                rotation = targetRot,
+                scale = targetScale,
+            },
+            start = new MyTransform
+            {
+                x = playerX,
+                y = playerY,
+                rotation = playerRot,
+                scale = playerScale
+            },
+            bgColor = bgColor,
+            spriteColor = spriteColor,
+            sprite = Sprites[0]
+        };
+    }
 }
