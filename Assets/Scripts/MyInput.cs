a78486e4ba504ac48a471c4bb51fd4c87b9ac501
atil <hyperion91@gmail.com>
2020-02-01 12:53:10 +0100

    Input stuff

--- Assets/Scripts/MyInput.cs
+++ Assets/Scripts/MyInput.cs
@@ -1,25 +1,61 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerInputs
+{
+    Left,
+    Right,
+    Up,
+    Down,
+    CW,
+    CounterCW,
+    ScaleUp,
+    ScaleDown
+}
+
+public enum InputTickResult
+{
+    Pending,
+    Confirm,
+    Timeout,
+}
+
 public class MyInput : MonoBehaviour
 {
-    //...
-    public enum PlayerInputs
-    {
-        Left,
-        Right,
-        Up,
-        Down,
-        CW,
-        CounterCW,
-        ScaleUp,
-        ScaleDown
-    }
+    public const float TimeLimit = 5f;
+    private float _timer;
 
     public List<PlayerInputs> myInputs;
 
-    public bool Tick()
+    public void Init()
     {
-        return false;
+        myInputs = new List<PlayerInputs>();
+        _timer = 0;
+    }
+
+    public InputTickResult Tick()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) myInputs.Add(PlayerInputs.Up);
+        if (Input.GetKeyDown(KeyCode.A)) myInputs.Add(PlayerInputs.Left);
+        if (Input.GetKeyDown(KeyCode.S)) myInputs.Add(PlayerInputs.Down);
+        if (Input.GetKeyDown(KeyCode.D)) myInputs.Add(PlayerInputs.Right);
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) myInputs.Add(PlayerInputs.CW);
+        if (Input.GetKeyDown(KeyCode.RightArrow)) myInputs.Add(PlayerInputs.CounterCW);
+        if (Input.GetMouseButtonDown(0)) myInputs.Add(PlayerInputs.ScaleDown);
+        if (Input.GetMouseButtonDown(1)) myInputs.Add(PlayerInputs.ScaleUp);
+
+        _timer += Time.deltaTime;
+
+        if (_timer > TimeLimit)
+        {
+            return InputTickResult.Timeout;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            return InputTickResult.Confirm;
+        }
+
+        return InputTickResult.Pending;
     }
 }
